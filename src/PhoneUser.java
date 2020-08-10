4954d60bd37fdc80c798a990404a0bbdaffe6d18
maulidiyahdhp180 <maulidiyahdhp180@gmail.com>
2020-08-10 22:32:40 +0700

    initial

---
+++ src/PhoneUser.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author puput
+ */
+public class PhoneUser {
+    private Phone phone;
+    
+    public PhoneUser(Phone phone){
+        this.phone = phone;
+    }
+    
+    void turnOnThePhone(){
+        this.phone.powerOn();
+    }
+    void turnOffThePhone(){
+        this.phone.powerOff();
+    }
+    void makePhoneLouder(){
+        this.phone.volumeUp();
+    }
+    void makePhoneSilent(){
+        this.phone.volumeDown();
+    }
+}
